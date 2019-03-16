a597840c94c98524fd63af7637bbe25269b030e2
Dimpal Paliwal <dimpalpaliwal1998@gmail.com>
2019-03-16 22:12:09 +0530

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -35,7 +35,7 @@
     <!-- ============================================================== -->
     <!-- signup form  -->
     <!-- ============================================================== -->
-    <form class="splash-container">
+    <form class="splash-container" action="registration.php" method="post">
         <div class="card">
             <div class="card-header">
                 <h3 class="mb-1">Registration Form</h3>
@@ -49,7 +49,10 @@
                     <input class="form-control form-control-lg" type="email" name="email" required="" placeholder="E-mail" autocomplete="off">
                 </div>
                 <div class="form-group">
-                    <input class="form-control form-control-lg" id="pass1" type="password" required="" placeholder="Password">
+                    <input class="form-control form-control-lg" id="pass1" name="password" type="password" required="" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <input class="form-control form-control-lg" id="tank_no" name="tank_no" type="number" required="" placeholder="number of tanks">
                 </div>
                 <!-- <div class="form-group">
                     <input class="form-control form-control-lg" required="" placeholder="Confirm">
@@ -57,6 +60,7 @@
                 <div class="form-group pt-2">
                     <button class="btn btn-block btn-primary" type="submit">Register My Account</button>
                 </div>
+                
                 <!-- <div class="form-group">
                     <label class="custom-control custom-checkbox">
                         <input class="custom-control-input" type="checkbox"><span class="custom-control-label">By creating an account, you agree the <a href="#">terms and conditions</a></span>
