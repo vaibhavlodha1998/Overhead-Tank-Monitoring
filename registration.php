8f4d40e9de9480f67a33e39861b83456f9cdc720
dimpalpaliwal <dimpalpaliwal1998@gmail.com>
2019-03-16 21:17:29 +0530

    Add files via upload
    database creation file for sign up page

---
+++ registration.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$con=mysqli_connect('localhost','root','','user');
+if($con)
+{	echo"connection successfull";}
+else
+{	echo"no connection";}
+msqli_select_db($con,'user');
+$name = $_POST['nick'];
+$email = $_POST['email'];
+$pass= $_POST['password'];
+$no_tank = $_POST['tank_no'];
+
+$q = "select * from user_details where name = '$name' && password = '$pass' ";
+$result=mysqli_query($con,$q);
+$num=mysqli_num_rows($result);
+if($num==1)
+{	echo"duplicate data";}
+else
+	{$qu="insert into user_details(name,email,password,no_tank) values ('$name','$email','$pass','$no_tank')";}
+mysqli_query($con,$qu);
+?>
